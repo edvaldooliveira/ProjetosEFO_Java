f20928178a85f9bfb5575343ee333488499c1245
Edvaldo Ferreira <ed.ferreiraoliveira@gmail.com>
2025-07-15 21:33:35 +0100

    Classe Aluno

---
+++ orientacaoobjetos/src/main/java/orientacaoobjetos/entities/Aluno.java
@@ -0,0 +1,49 @@
+package orientacaoobjetos.entities;
+
+public class Aluno {
+    private String nome;
+    private Integer idade;
+    private String matricula;
+
+    public Aluno(){
+    }
+
+    public Aluno(String nome, Integer idade, String matricula) {
+        this.nome = nome;
+        this.idade = idade;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", matricula='" + matricula + '\'' +
+                '}';
+    }
+}
